b256298688e022d64d65ae2837764036c14f176b
govind sharma <govindgovind852@gmail.com>
2022-11-03 09:03:26 +0000

    room-joining added

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT;
 server.listen(PORT, () => {
   socketHandler();
   console.log("app running on port " + PORT);
-});
+})
--- src/SocketApp.js
+++ src/SocketApp.js
@@ -2,10 +2,22 @@ import { io } from "./App.js";
 
 export const socketHandler = () => {
   io.use(async (socket, next) => {
-    next();
+    if(process.env.TOKEN===socket.handshake.auth.token){
+      return next()
+    }
+    else{
+      socket.disconnect()
+      return next(new Error("Auth failed"));
+    }
   });
 
   io.on("connection", async (socket) => {
+    const {name,role,room}=socket.handshake.query
+
+    socket.join(room)
+
+    socket.to(room).emit("user-joined",{name,role})
+
     socket.emit("connected", { id: socket.id });
   });
 };
